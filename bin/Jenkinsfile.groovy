ea67b81dab6361dd8e4f092d1c8448b10bf92e0d
Sergiy Rayevskyy <srayevskyy@srayevskyy-mlt.nvidia.com>
2021-10-31 23:25:19 -0700

    Added Jenkinsfile

---
+++ bin/Jenkinsfile.groovy
@@ -0,0 +1,30 @@
+pipeline {
+    agent any
+    stages {
+        stage('MySQL in docker') {
+            agent {
+                docker {
+                    image 'mysql/mysql-server:5.7'
+                    args '--entrypoint='
+                }
+            }
+            steps {
+                sh 'mysql --version'
+                sh 'ls -al'
+            }
+        }
+
+        stage('AWS cli in docker') {
+            agent {
+                docker {
+                    image 'amazon/aws-cli:2.3.1'
+                    args '--entrypoint='
+                }
+            }
+            steps {
+                sh 'aws --version'
+                sh 'ls -al'
+            }
+        }
+    }
+}
