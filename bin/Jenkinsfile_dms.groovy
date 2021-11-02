448070de7e0e3bee52bee08e290d43c897cb2048
Sergiy Rayevskyy <srayevskyy@srayevskyy-mlt.nvidia.com>
2021-11-02 11:23:34 -0700

    Added dms Jenkinsfile

---
+++ bin/Jenkinsfile_dms.groovy
@@ -0,0 +1,11 @@
+pipeline {
+    agent any
+    stages {
+        stage('AWS cli in docker') {
+            steps {
+                sh 'aws --version'
+                sh 'ls -al'
+            }
+        }
+    }
+}
