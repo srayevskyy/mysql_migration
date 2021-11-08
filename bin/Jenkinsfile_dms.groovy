8b5c9c91c21dc71620eff056220af85de3411d52
Sergiy Rayevskyy <srayevskyy@srayevskyy-mlt.nvidia.com>
2021-11-08 12:29:49 -0800

    Parameterized endpoint ARNs

--- bin/Jenkinsfile_dms.groovy
+++ bin/Jenkinsfile_dms.groovy
@@ -11,9 +11,9 @@ pipeline {
                 sh '''
 aws dms create-replication-task \
 --replication-task-identifier "mysql-src-to-dst-cli" \
---source-endpoint-arn "arn:aws:dms:us-west-2:627223132631:endpoint:NMWATSHBNY7SGWSU5PQ26O6RPYW7DCU7ND6PPRQ" \
---target-endpoint-arn "arn:aws:dms:us-west-2:627223132631:endpoint:3TYRTDFS7CFBYF7ZK2QSD3EDKZ7POOL3RBMEX5Q" \
---replication-instance-arn "arn:aws:dms:us-west-2:627223132631:rep:NABEOHCFPWNOG5N2J54TLPQR562BHVZGZF34OOI" \
+--source-endpoint-arn "${SOURCE_ENDPOINT_ARN}" \
+--target-endpoint-arn "${TARGET_ENDPOINT_ARN}" \
+--replication-instance-arn "${REPLICATION_INSTANCE_ARN}" \
 --migration-type "full-load-and-cdc" \
 --table-mappings https://raw.githubusercontent.com/srayevskyy/mysql_migration/main/infra/terraform/rds/table_mappings.json
                 '''
