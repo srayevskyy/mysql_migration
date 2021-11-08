7ef15de482e60fbcb27f97720f4c8069a47bb248
Sergiy Rayevskyy <srayevskyy@srayevskyy-mlt.nvidia.com>
2021-11-08 11:53:27 -0800

    Updated jenkinsfile

--- bin/Jenkinsfile.groovy
+++ bin/Jenkinsfile.groovy
@@ -5,19 +5,15 @@ pipeline {
     }
     stages {
 
-        stage('test') {
-            steps {
-                sh 'whoami'
-            }
-        }
-
         stage('MySQL in docker') {
+            /*
             agent {
                 docker {
                     image 'mysql/mysql-server:5.7'
                     args '--entrypoint='
                 }
             }
+            */
             steps {
                 sh 'mysql --version'
                 sh 'ls -al'
