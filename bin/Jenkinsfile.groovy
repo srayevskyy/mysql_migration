4a631be3cde4c61ac7832737b8b4276b1e50bae5
Sergiy Rayevskyy <srayevskyy@srayevskyy-mlt.nvidia.com>
2021-11-08 14:34:32 -0800

    Extra code cleanup

--- bin/Jenkinsfile.groovy
+++ bin/Jenkinsfile.groovy
@@ -4,36 +4,11 @@ pipeline {
         MYSQL_ROOT_PASSWORD = credentials('MYSQL_ROOT_PASSWORD')
     }
     stages {
-
         stage('MySQL in docker') {
-            /*
-            agent {
-                docker {
-                    image 'mysql/mysql-server:5.7'
-                    args '--entrypoint='
-                }
-            }
-            */
             steps {
                 sh 'mysql --version'
-                sh 'ls -al'
                 sh "./bin/release.sh ${MYSQL_DST_HOST} 3306 root ${MYSQL_ROOT_PASSWORD} dms_sample"
             }
         }
-
-        /*
-        stage('AWS cli in docker') {
-            agent {
-                docker {
-                    image 'amazon/aws-cli:2.3.1'
-                    args '--entrypoint='
-                }
-            }
-            steps {
-                sh 'aws --version'
-                sh 'ls -al'
-            }
-        }
-        */
     }
 }
