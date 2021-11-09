75df4a9dd368eedda2774d82f525ebcf8e39237b
Sergiy Rayevskyy <srayevskyy@srayevskyy-mlt.nvidia.com>
2021-11-09 14:12:25 -0800

    updated Jenkinsfile

--- bin/Jenkinsfile.deploy.target.db.structure.groovy
+++ bin/Jenkinsfile.deploy.target.db.structure.groovy
@@ -7,8 +7,8 @@ pipeline {
                 sh '''
                 set -x
                 wget --no-clobber --directory-prefix=${USER} https://s3.amazonaws.com/rds-downloads/rds-ca-2019-root.pem
-                MYSQL_TOKEN="$(aws rds generate-db-auth-token --hostname $MYSQL_DST_HOST --port 3306 --region us-west-2 --username iam_admin)"
-                ./bin/release.sh ${MYSQL_DST_HOST} 3306 iam_admin ${MYSQL_TOKEN} dms_sample
+                MYSQL_TOKEN="$(aws rds generate-db-auth-token --hostname $MYSQL_DST_HOST --port 3306 --region us-west-2 --username iam_admin)"                
+                mysql --host=$MYSQL_DST_HOST --port=3306 --ssl-ca=${USER}/rds-ca-2019-root.pem --user=iam_admin --password=$MYSQL_TOKEN -e "SELECT 1+1"
                 '''
                 // mysql --host=$MYSQL_DST_HOST --port=3306 --ssl-ca=${USER}/rds-ca-2019-root.pem --user=iam_admin --password=$MYSQL_TOKEN -e "SELECT 1+1"
             }
