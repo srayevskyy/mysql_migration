f6e96b719991b1651c06ad44a6ec010640e7b5a9
Sergiy Rayevskyy <srayevskyy@srayevskyy-mlt.nvidia.com>
2021-11-09 14:19:16 -0800

    Updated jenkinsfile

--- bin/Jenkinsfile.deploy.target.db.structure.groovy
+++ bin/Jenkinsfile.deploy.target.db.structure.groovy
@@ -8,7 +8,7 @@ pipeline {
                 set -x
                 wget --no-clobber --directory-prefix=${USER} https://s3.amazonaws.com/rds-downloads/rds-ca-2019-root.pem
                 export MYSQL_TOKEN="$(aws rds generate-db-auth-token --hostname $MYSQL_DST_HOST --port 3306 --region us-west-2 --username iam_admin)"                
-                echi ${MYSQL_TOKEN}
+                echo ${MYSQL_TOKEN}
                 ./bin/release.sh ${MYSQL_DST_HOST} 3306 iam_admin dms_sample
                 '''
             }
