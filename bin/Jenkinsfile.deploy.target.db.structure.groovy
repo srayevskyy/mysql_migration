74447471ec68d1c6ef138432079dfb41ee9a8e99
Sergiy Rayevskyy <srayevskyy@srayevskyy-mlt.nvidia.com>
2021-11-09 13:59:19 -0800

    Updated Jenkinsfile

--- bin/Jenkinsfile.deploy.target.db.structure.groovy
+++ bin/Jenkinsfile.deploy.target.db.structure.groovy
@@ -1,20 +1,23 @@
-def getMySQLToken(username) {
-    return sh (
-        script: "aws rds generate-db-auth-token --hostname $MYSQL_DST_HOST --port 3306 --region us-west-2 --username iam_admin",
-        returnStdout: true
-    ).trim()
-}
+// def getMySQLToken(username) {
+//     return sh (
+//         script: "aws rds generate-db-auth-token --hostname $MYSQL_DST_HOST --port 3306 --region us-west-2 --username iam_admin",
+//         returnStdout: true
+//     ).trim()
+// }
 
 pipeline {
     agent any
-    environment {
-        MYSQL_TOKEN = getMySQLToken("iam_admin")
-    }
+    // environment {
+    //     MYSQL_TOKEN = getMySQLToken("iam_admin")
+    // }
     stages {
         stage('MySQL in docker') {
             steps {
-                sh "echo ${MYSQL_TOKEN}"
-                sh "mysql --host=${MYSQL_DST_HOST} --port=3306 --ssl-ca=${USER}/rds-ca-2019-root.pem --user=iam_admin --password=${MYSQL_TOKEN} -e 'SELECT 1+1'"
+                sh '''
+                ls -al
+                '''
+                // sh "echo ${MYSQL_TOKEN}"
+                // sh "mysql --host=${MYSQL_DST_HOST} --port=3306 --ssl-ca=${USER}/rds-ca-2019-root.pem --user=iam_admin --password=${MYSQL_TOKEN} -e 'SELECT 1+1'"
                 // sh "./bin/release.sh ${MYSQL_DST_HOST} 3306 root ${MYSQL_ROOT_PASSWORD} dms_sample"
             }
         }
