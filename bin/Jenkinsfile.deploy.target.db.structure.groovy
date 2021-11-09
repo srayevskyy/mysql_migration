4996066ebffff3c0ed63ef4a3ce81a4442b8c248
Sergiy Rayevskyy <srayevskyy@srayevskyy-mlt.nvidia.com>
2021-11-09 14:26:58 -0800

    Updated Jenkinsfile

--- bin/Jenkinsfile.deploy.target.db.structure.groovy
+++ bin/Jenkinsfile.deploy.target.db.structure.groovy
@@ -1,15 +1,17 @@
-
 pipeline {
     agent any
+    environment {
+        MYSQL_USER = "iam_admin"
+    }
     stages {
         stage('Deploy target DB structure and/or truncate tables') {
             steps {
                 sh '''
                 set -x
                 wget --no-clobber --directory-prefix=${HOME} https://s3.amazonaws.com/rds-downloads/rds-ca-2019-root.pem
-                export MYSQL_TOKEN="$(aws rds generate-db-auth-token --hostname $MYSQL_DST_HOST --port 3306 --region us-west-2 --username iam_admin)"                
+                export MYSQL_TOKEN="$(aws rds generate-db-auth-token --hostname $MYSQL_DST_HOST --port 3306 --region us-west-2 --username ${MYSQL_USER})"
                 echo ${MYSQL_TOKEN}
-                ./bin/release.sh ${MYSQL_DST_HOST} 3306 iam_admin dms_sample
+                ./bin/release.sh ${MYSQL_DST_HOST} 3306 ${MYSQL_USER} dms_sample
                 '''
             }
         }
