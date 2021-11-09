f2eae6a4eabe0354808c8e83d1d3ae6d4bb1c10e
Sergiy Rayevskyy <srayevskyy@srayevskyy-mlt.nvidia.com>
2021-11-09 14:01:43 -0800

    Updated Jenkinsfile

--- bin/Jenkinsfile.deploy.target.db.structure.groovy
+++ bin/Jenkinsfile.deploy.target.db.structure.groovy
@@ -11,10 +11,13 @@ pipeline {
     //     MYSQL_TOKEN = getMySQLToken("iam_admin")
     // }
     stages {
-        stage('MySQL in docker') {
+        stage('Deploy target DB structure and/or truncate tables') {
             steps {
                 sh '''
-                ls -al
+                wget --no-clobber --directory-prefix=${USER} https://s3.amazonaws.com/rds-downloads/rds-ca-2019-root.pem
+                MYSQL_TOKEN="$(aws rds generate-db-auth-token --hostname $MYSQL_DST_HOST --port 3306 --region us-west-2 --username iam_admin)"
+                echo $MYSQL_TOKEN
+                mysql --host=$MYSQL_DST_HOST --port=3306 --ssl-ca=${USER}/rds-ca-2019-root.pem --user=iam_admin --password=$MYSQL_TOKEN
                 '''
                 // sh "echo ${MYSQL_TOKEN}"
                 // sh "mysql --host=${MYSQL_DST_HOST} --port=3306 --ssl-ca=${USER}/rds-ca-2019-root.pem --user=iam_admin --password=${MYSQL_TOKEN} -e 'SELECT 1+1'"
